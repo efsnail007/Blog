201385061c2c34ad72c858151e3b4fbd30121f3c
snail <sgefsnkiospch908@gmail.com>
2025-09-25 10:01:22 +0300

    job

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,34 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    all_posts = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(all_posts, 3)
+    page_number = request.GET.get("page")
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, "blog/post/detail.html", {"post": post})
