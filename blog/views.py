022cc1da3432d0af1ca3dccd334c4122204d478f
snail <sgefsnkiospch908@gmail.com>
2025-09-30 09:27:25 +0300

    job

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
+from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
+from .forms import CommentForm, EmailPostForm
 from .models import Post
 
 
@@ -24,7 +27,7 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
+    post_obj = get_object_or_404(
         Post,
         status=Post.Status.PUBLISHED,
         slug=post,
@@ -32,7 +35,61 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+    # Список активных комментариев к этому посту
+    comments = post_obj.comments.filter(active=True)
+    # Форма для комментирования пользователями
+    form = CommentForm()
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post_obj, "comments": comments, "form": form},
+    )
+
+
+def post_share(request, post_id):
+    # Извлечь пост по его идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s ({cd['email']}) comments: {cd['comments']}"
+            )
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd["to"]])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Создать объект класса Comment, не сохраняя его в базе данных
+        comment = form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        # Сохранить комментарий в базе данных
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
+    )
 
 
 class PostListView(ListView):
--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Конфигурация сервера электронной почты
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "your_account@gmail.com"
+EMAIL_HOST_PASSWORD = "xxxxxxxxxxxxxxxx"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
